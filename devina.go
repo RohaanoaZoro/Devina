d4d10d766a38bdcc0c55fee19e60f2ae346e40e6
Rohaan Almeida <31466702+RohaanoaZoro@users.noreply.github.com>
2021-01-21 13:54:56 +0530

    Added Feature to get commits only after preProd/Prod commit

--- devina.go
+++ devina.go
@@ -10,18 +10,21 @@ import (
 var owner string = "lakbhat"
 var repo string = "SententiaRepo"
 var sha string = "dev_ui_editor"
-var bearer = "Bearer " + "3b20ea4fe651a1882c8847082478469251f56ffc"
+var bearer = "Bearer " + "e3c16b280dad763681fe1efda88e66e2e0492b97"
 var url string = "https://api.github.com/repos/lakbhat/SententiaRepo/"
+var since string = ""
 
 type GitResStruct struct {
-	Sha         string          `json:"sha,omitempty"`
+	Sha         string          `json:"sha ,omitempty"`
 	NodeId      string          `json:"node_id,omitempty"`
 	Commit      commitStruct    `json:"commit,omitempty"`
+	Committer   authorStruct    `json:"committer,omitempty"`
+	Message     string          `json:"message,omitempty"`
+	Tree        treeStruct      `json:"tree,omitempty"`
 	Url         string          `json:"url,omitempty"`
 	HtmlUrl     string          `json:"html_url,omitempty"`
 	CommentsUrl string          `json:"comments_url,omitempty"`
 	Author      authorStruct    `json:"author,omitempty"`
-	Committer   authorStruct    `json:"committer,omitempty"`
 	Parents     []parentsStruct `json:"parents,omitempty"`
 }
 
@@ -44,7 +47,7 @@ type commitAuthorStruct struct {
 type commitCommitterStruct struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
-	Data  string `json:"date"`
+	Date  string `json:"date"`
 }
 
 type treeStruct struct {
@@ -86,19 +89,41 @@ type parentsStruct struct {
 	HtmlURL string `json:"html_url"`
 }
 
+type UsefulJsonInfo struct {
+	Sha        string `json:"sha"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	Date       string `json:"date"`
+	Message    string `json:"message"`
+	parentsSha string `json:"sha"`
+}
+
 func main() {
 
-	getCommits("dev_rule_engine", "1")
+	// var lastDate string = getLastDateInPreProd("prod_ui_editor")
+	// log.Println(lastDate)
+	// getCommits("dev_ui_editor", "1", "2021-01-01T12:13:43Z")
+	getCommits("preprod_ui_editor", "1", "2021-01-19T16:40:04Z")
+	// getCommits("preprod_ui_editor", "1", lastDate)
 }
 
-func getCommits(branch string, perPage string) {
+func getCommits(branch string, perPage string, since string) {
 
-	var tempUrl string = url + "commits?per_page=" + perPage + "&sha=" + branch
-	makeGetRequest(tempUrl)
+	var tempUrl string = url + "commits?sha=" + branch + "&since=" + since
+	var resBody []byte = makeGetRequest(tempUrl)
+	uji := getUsefulInfo(resBody)
+	log.Println(uji)
 
 }
 
-func makeGetRequest(url string) []GitResStruct {
+func getLastDateInPreProd(branch string) string {
+
+	var tempUrl string = url + "commits?per_page=1&sha=" + branch
+	var resBody []byte = makeGetRequest(tempUrl)
+	return getLastDate(resBody)
+}
+
+func makeGetRequest(url string) []byte {
 	// make a sample HTTP GET request
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Add("Authorization", bearer)
@@ -117,8 +142,39 @@ func makeGetRequest(url string) []GitResStruct {
 
 	body, _ := ioutil.ReadAll(resp.Body)
 
-	var gitResCollection []GitResStruct
-	err = json.Unmarshal(body, &gitResCollection)
+	return body
+}
 
-	return gitResCollection
+func getUsefulInfo(resBody []byte) []UsefulJsonInfo {
+
+	var gitResCollection []GitResStruct
+	err := json.Unmarshal(resBody, &gitResCollection)
+
+	if err != nil {
+		log.Println("Error While UnMarshalling")
+	}
+
+	var uji []UsefulJsonInfo
+
+	for i := 0; i < len(gitResCollection); i++ {
+		// log.Println(gitResCollection[i])
+		log.Println(gitResCollection[i].Commit.Message)
+		var singleuji UsefulJsonInfo = UsefulJsonInfo{Sha: gitResCollection[i].Commit.Tree.Sha, Name: gitResCollection[i].Commit.Author.Name, Email: gitResCollection[i].Commit.Author.Email, Date: gitResCollection[i].Commit.Author.Date, Message: gitResCollection[i].Commit.Message, parentsSha: gitResCollection[i].Parents[0].Sha}
+
+		uji = append(uji, singleuji)
+	}
+
+	return uji
+}
+
+func getLastDate(resBody []byte) string {
+
+	var gitResCollection []GitResStruct
+	err := json.Unmarshal(resBody, &gitResCollection)
+
+	if err != nil {
+		log.Println("Error While UnMarshalling")
+	}
+
+	return gitResCollection[0].Commit.Author.Date
 }
